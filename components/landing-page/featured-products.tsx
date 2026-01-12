172185db3aa5981e02c95ee18f4e95f7da0f03e8
Jean-Aime-2024 <jeanaime1000@gmail.com>
2026-01-12 16:03:27 +0200

    [fatal] : adding drizzle and data fetching

--- components/landing-page/featured-products.tsx
+++ components/landing-page/featured-products.tsx
@@ -3,27 +3,30 @@ import SectionHeader from '../common/section-header';
 import { Button } from '../ui/button';
 import Link from 'next/link';
 import ProductCard from '../products/product-card';
+import { getFeaturedProducts } from '@/lib/products/product-select';
 
-const featuredProducts = [
-  {
-    id: 1,
-    name: 'ParityKit',
-    description: 'A toolkit for creating parity products',
-    tags: ['Saas', 'Pricing', 'Global'],
-    votes: 615,
-    isFeatured: true
-  },
-  {
-    id: 2,
-    name: 'Modern Full Stack Next.js Course',
-    description: 'Learn to build production-ready full stack apps with Next.js',
-    tags: ['Next.js', 'PricingFull-Stack', 'Course'],
-    votes: 124,
-    isFeatured: false
-  },
-];
+// const featuredProducts = [
+//   {
+//     id: 1,
+//     name: 'ParityKit',
+//     description: 'A toolkit for creating parity products',
+//     tags: ['Saas', 'Pricing', 'Global'],
+//     votes: 615,
+//     isFeatured: true
+//   },
+//   {
+//     id: 2,
+//     name: 'Modern Full Stack Next.js Course',
+//     description: 'Learn to build production-ready full stack apps with Next.js',
+//     tags: ['Next.js', 'PricingFull-Stack', 'Course'],
+//     votes: 124,
+//     isFeatured: false
+//   },
+// ];
 
-const FeaturedProducts = () => {
+const FeaturedProducts = async () => {
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <section className="py-20 bg-muted/20">
       <div className="wrapper">
