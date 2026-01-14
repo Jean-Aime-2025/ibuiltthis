57d96433c72fa89174283038e742a14826c6ecc0
Jean-Aime-2024 <jeanaime1000@gmail.com>
2026-01-14 16:39:45 +0200

    [fatal] : explore page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,24 @@
-const ExploreProducts = () => {
-  return (
-    <div>ExploreProducts</div>
-  )
-}
+"use cache";
+import SectionHeader from "@/components/common/section-header";
+import ProductExplorer from "@/components/products/product-explorer";
+import { getAllApprovedProducts } from "@/lib/products/product-select";
+import { CompassIcon } from "lucide-react";
 
-export default ExploreProducts
+export default async function ExplorePage() {
+  const products = await getAllApprovedProducts();
+
+  return (
+    <div className="py-20">
+      <div className="wrapper">
+        <div className="mb-12">
+          <SectionHeader
+            title="Explore All Products"
+            icon={CompassIcon}
+            description="Browse and discover amazing projects from our community"
+          />
+        </div>
+        <ProductExplorer products={products} />
+      </div>
+    </div>
+  );
+}
